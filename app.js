291d04d0828bc193f3f4938aa0ead220bd1d8b01
Chloe-O <chloeosbourne@yahoo.co.uk>
2025-07-04 19:04:55 +0100

    adding modal functionality

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const submitListItemBtn = document.getElementById("listSubmit");
 const list = document.getElementById("list");
 const clearDataBtn = document.getElementById("clearData");
 
+const modal = document.getElementById("confirmDataClearModal");
+const modalBtnsContainer = document.getElementById("#btn-wrap");
+const deleteDataBtn = document.getElementById("deleteData");
+const keepDataBtn = document.getElementById("keepData");
+
 let listArr = [];
 
 const stringifiedArr = JSON.stringify(listArr);
+
+// To implement
+
+/*
+- add modal to confirm user wants to delete data
+- click on list item adds strike through and adds 'complete' (class or data-val?)
+- add individual delete item button for each list item
+*/
 
 function saveListToLocalStorage(arr) {
   localStorage.setItem("todoList", arr);
@@ -32,8 +45,15 @@ submitListItemBtn.addEventListener("click", (e) => {
 
 // user wants to clear all list items
 function clearAllData() {
-  list.innerHTML = "";
-  localStorage.removeItem("todoList");
+  modal.style.display = "flex";
+  modalBtnsContainer.addEventListener("click", (e) => {
+    if ((e.target.value = "delete")) {
+      // list.innerHTML = "";
+      // localStorage.removeItem("todoList");
+      console.log(e.target.value);
+
+    }
+  });
 }
 
 // user wants to retrieve list items when document has loaded
