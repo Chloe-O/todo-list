bf542e75eebd61463cee0b19a71455c045d3ddc3
Chloe-O <chloeosbourne@yahoo.co.uk>
2025-07-04 07:08:18 +0100

    list stored in local storage

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const listInput = document.getElementById("listInput");
 const submitListItemBtn = document.getElementById("listSubmit");
 const list = document.getElementById("list");
+const clearDataBtn = document.getElementById("clearData");
 
 let listArr = [];
+
+const stringifiedArr = JSON.stringify(listArr);
+
+function saveListToLocalStorage(arr) {
+  localStorage.setItem("todoList", arr);
+  console.log(arr);
+}
 
 function renderListItems(x) {
   let NewItemHTML = document.createElement("li");
@@ -18,8 +26,18 @@ submitListItemBtn.addEventListener("click", (e) => {
   if (listInput.value) {
     let newItem = listInput.value;
     listArr.push(newItem);
+    saveListToLocalStorage(listArr);
     renderListItems(newItem);
-
     listInput.value = "";
   }
 });
+
+// user wants to clear all tasks
+function clearAllData() {
+  console.log("testing!");
+  localStorage.removeItem("todoList");
+}
+
+function getListFromStorage() {}
+
+clearDataBtn.addEventListener("click", clearAllData);
